718bfb512b18ebfa51b3379c8d3a77109fbe030c
Hongmo98 <hongmo0909@outlook.com>
2020-05-19 13:11:25 +0700

    test pgp

--- controller/bankController.js
+++ controller/bankController.js
@@ -86,25 +86,76 @@ module.exports = {
 
     testPgp: async (req, res, next) => {
 
-        const passphrase = `123456`;
-        await openpgp.initWorker({ path: 'openpgp.worker.js' }) // set the relative web worker path
+        const privateKeyArmored = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+Version: OpenPGP.js v4.10.4
+Comment: https://openpgpjs.org
 
+xYYEXsNV0xYJKwYBBAHaRw8BAQdA7LA4kqpEv+5hkH1Q3ewjHYMJNQRLKWWb
+vvWUkIQuh3b+CQMIB6vnI49UfdHgUyox1KJSDjwbSmXXiiXlKOLKgPUhafRV
+x6yqjp4pOrkLMF6wGCn5BrLzOyKUI8of4yy+B5PRK+Ulfb/DojTzeD3sNyh3
+Yc0fTVBCYW5rIDxob25nbW8yNDExOThAZ21haWwuY29tPsJ4BBAWCgAgBQJe
+w1XTBgsJBwgDAgQVCAoCBBYCAQACGQECGwMCHgEACgkQrsP2+0GwTATExQD9
+Hv5exNhzeoGJQeYiKn97Xh8uOHoUVrYOBC6KWqk3ahIBAO11EtAM2vR8AW8p
+j8iv0DCP35SDDlhj0B3dEwtbrXECx4sEXsNV0xIKKwYBBAGXVQEFAQEHQD+R
+D2NClzWFUzj0acl9GWL7IReHH5YkPEUPduEkr8YoAwEIB/4JAwjqUP7JgODk
+O+AA4FxM61djyKPfruHfIeI3i1AN9u0XLh6L1e73HaW3aZIOqaX0aVvxBg3x
+vQLV2yDcs7oKp4OZn4V5VcB+REVYSSteXV7ewmEEGBYIAAkFAl7DVdMCGwwA
+CgkQrsP2+0GwTASNcQEAlCqApG/iNUFsLpMURqwqYtnO6JeyBmKJS//J0uNW
+WP8A/0yACDTK5PjAsXnnGIt1t9moZpjGUp8FTFHv3JpxXd4G
+=1zQz
+-----END PGP PRIVATE KEY BLOCK-----`
+        const publicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: OpenPGP.js v4.10.4
+Comment: https://openpgpjs.org
+
+xjMEXsNV0xYJKwYBBAHaRw8BAQdA7LA4kqpEv+5hkH1Q3ewjHYMJNQRLKWWb
+vvWUkIQuh3bNH01QQmFuayA8aG9uZ21vMjQxMTk4QGdtYWlsLmNvbT7CeAQQ
+FgoAIAUCXsNV0wYLCQcIAwIEFQgKAgQWAgEAAhkBAhsDAh4BAAoJEK7D9vtB
+sEwExMUA/R7+XsTYc3qBiUHmIip/e14fLjh6FFa2DgQuilqpN2oSAQDtdRLQ
+DNr0fAFvKY/Ir9Awj9+Ugw5YY9Ad3RMLW61xAs44BF7DVdMSCisGAQQBl1UB
+BQEBB0A/kQ9jQpc1hVM49GnJfRli+yEXhx+WJDxFD3bhJK/GKAMBCAfCYQQY
+FggACQUCXsNV0wIbDAAKCRCuw/b7QbBMBI1xAQCUKoCkb+I1QWwukxRGrCpi
+2c7ol7IGYolL/8nS41ZY/wD/TIAINMrk+MCxeecYi3W32ahmmMZSnwVMUe/c
+mnFd3gY=
+=6NYp
+-----END PGP PUBLIC KEY BLOCK-----`;
+        const revocationCertificate = ` -----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: OpenPGP.js v4.10.4
+Comment: https://openpgpjs.org
+Comment: This is a revocation certificate
+
+wmEEIBYKAAkFAl7DVdMCHQAACgkQrsP2+0GwTAQTfwEA/r+lyNn6zEjlGfN/
+7vGIvPBnVsHvFKkamOQNCXnsZQcBAM+G4s3GYhHeTbdzBxTCb3m4XHXbpiQj
+LCo24wxpqZEB
+=NTmt
+-----END PGP PUBLIC KEY BLOCK-----`
+        const passphrase = `nguyen thi hong mo`;
+        // const passphrase = `hongmo`;
+        await openpgp.initWorker({ path: 'openpgp.worker.js' })
+        const { keys: [privateKey] } = await openpgp.key.readArmored(privateKeyArmored);
+        await privateKey.decrypt(passphrase);
         const options = {
-            message: openpgp.message.fromText('hello'),       // input as Message object
-            publicKeys: (await openpgp.key.readArmored(partner.publicKeyArmored)).keys, // for encryption
+            message: openpgp.message.fromText('hello phi'),
+            publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys,
+            privateKeys: [privateKey]
+
         }
         console.log(options);
+
         const { data: encrypted } = await openpgp.encrypt(options);
 
-        res.status(200).json({ result: true });
+        const { data: decrypted } = await openpgp.decrypt({
+            message: await openpgp.message.readArmored(encrypted),              // parse armored message
+            publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys, // for verification (optional)
+            privateKeys: [privateKey]                                           // for decryption
+        });
+        res.status(200).json({ result: decrypted });
 
 
-    }
-
-
-
+    },
 
 
 }
+
 
 
--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const bankController = require('../controller/bankController')
 
 router.get('/accountNumber', bankController.queryAccountInformation);
-router.post('/transfer', bankController.transactionAccount)
-router.post('/transaction', bankController.testPgp)
+router.post('/transfer', bankController.transactionAccount);
+router.post('/transaction', bankController.testPgp);
 
 module.exports = router;
